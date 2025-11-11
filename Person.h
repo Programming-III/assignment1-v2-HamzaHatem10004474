3cac41832c4f80818a3c65a5f26e1714be417003
HamzaHatem10004474 <hamza.hatem.salah@gmail.com>
2025-11-11 09:57:41 +0200

    Updated Person.h

--- Person.h
+++ Person.h
@@ -3,18 +3,43 @@
 
 #include <string>
 using namespace std;
-
 //#write Person class here
+class Person{
+    private:
+        string name;
+        int id;
+    public:
+        
+        void display();
+
+        Person(){
+            name = " ";
+            id = 0; 
+        }
+
+        Person(string name, int id){
+            this -> name = name;
+            this -> id = id;
+        }
+
+        ~Person(){
+            
+        }
+
+        void set_name(string name){
+            this -> name = name;
+        }
+
+        
+
+        int get_id(){
+            return id;
+        }
+
+        string get_name(){
+            return name;
+        }
 
 
-
-
-
-
-
-
-
-
-
-
+}
 #endif
