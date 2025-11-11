8a4ada4faf1ac30d28c69a86c88c700fe88eec4d
HamzaHatem10004474 <hamza.hatem.salah@gmail.com>
2025-11-11 09:58:17 +0200

    Updated Student.h

--- Student.h
+++ Student.h
@@ -3,19 +3,48 @@
 
 #include "Person.h"
 #include <string>
-#using namespace std;
+using namespace std;
 //#write student class here
+class Student : public Person {
+    private:
+        int yearLevel;
+        string major;
+    public:
+        Student(){
+            yearLevel = 0;
+            major = " ";
+        }
 
+        Student(int yearLevel, string major){
+            this -> yearLevel = yearLevel;
+            this -> major = major;
+        }
 
+        ~Student(){
 
+        }
 
+        void set_major(string major){
+            this-> major = major;
+        }
 
+        string get_major(){
+            return major;
+        }
 
+        void set_yearLevel(int yearLevel){
+            this -> yearLevel = yearLevel;
+        }
 
+        int get_yearLevel(){
+            return yearLevel;
+        }
 
+        void display(){
+            Person::display();
+            cout << "Year: " << yearLevel << endl;
+            cout << "Major: " << major << endl;
+        }
 
-
-
-
-
+};
 #endif
