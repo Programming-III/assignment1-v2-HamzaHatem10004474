00adffda9f1e7ca2e65440a388b879b602e82154
HamzaHatem10004474 <hamza.hatem.salah@gmail.com>
2025-11-11 09:56:54 +0200

    updated Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -2,18 +2,40 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
+#include <string>
+
 using namespace std;
 //#write Instructor class here
+class Instructor : public Person{
+    private:
+        string department;
+        int experienceYears;
+    public:
+        void display(){
+            Person::display();
+            cout << "Department: " << department << endl;
+            cout << "Experience: " << experienceYears << endl;
+        }
 
+        Instructor(){
+            department = " ";
+            experienceYears = 0; 
+        }
 
+        Instructor(string department,int experienceYears){
+            this-> department = department;
+            this-> experienceYears = experienceYears;
+        }
 
+        ~Instructor(){
 
+        }
 
+        void display(){
+            Person::display();
+            cout << "Department: " << department << endl;
+            cout << "Experience: " << experienceYears << endl;
+        }
 
-
-
-
-
-
-
+};
 #endif
